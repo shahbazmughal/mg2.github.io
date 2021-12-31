6ba0d042bd7e1022ed3c65ce9d0cb8b8b9178b4e
shahbazmughal <shahbaz.mughal@hostingshouse.com>
2021-12-31 20:21:24 +0500

    Mumara SMS Features Details Pages Done

---
+++ e2070dfd-d6a41fd4781ed6e02890.js
@@ -0,0 +1,2 @@
+(self.webpackChunk_elegantstack_gatsby_starter_flexiblocks=self.webpackChunk_elegantstack_gatsby_starter_flexiblocks||[]).push([[4794],{1184:function(t,a,e){"use strict";e.d(a,{OrA:function(){return r}});var s=e(8575);function r(t){return(0,s.w_)({tag:"svg",attr:{viewBox:"0 0 24 24"},child:[{tag:"path",attr:{d:"M16.293 9.293L12 13.586 7.707 9.293 6.293 10.707 12 16.414 17.707 10.707z"}}]})(t)}}}]);
+//# sourceMappingURL=e2070dfd-d6a41fd4781ed6e02890.js.map
